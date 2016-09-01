5196c2374aa6e32005c42e33ecd6c6afe757a13e
Scott Jacobsen <jacobsenscott@yahoo.com>
2016-08-31 18:09:30 -0600

    Use fakeredis
    `brew uninstall redis` - thank the maker.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require "lita-circleci-commander"
 require "lita/rspec"
 require "pry"
+require "fakeredis"
 
 # A compatibility mode is provided for older plugins upgrading from Lita 3. Since this plugin
 # was generated with Lita 4, the compatibility mode should be left disabled.
